28ea1be1226e5475b4ee877fe1c9cc9a1d456224
leon3108 <maxime.noel@epitech.eu>
2024-05-23 18:29:49 +0200

    feat: update footer ui and improves features for player

--- src/components/ui/footer/footer.tsx
+++ src/components/ui/footer/footer.tsx
@@ -1,26 +1,29 @@
+import { CircleCheck, Volume2 } from 'lucide-react'
 import Image from 'next/image'
 import { Slider } from '../slider'
+import Player from './player'
 import yourEpisodes from '/public/yourEpisodes.png'
 
 export default function Footer() {
   return (
-    <footer className="w-full h-20 flex justify-between">
-      <div className="flex">
-        <Image src={yourEpisodes} alt="song's cover" />
-        <div className="flex flex-col">
+    <footer className="flex h-20 w-full justify-between ">
+      <div className="flex w-1/3 space-x-2">
+        <Image src={yourEpisodes} alt="song's cover" className="rounded-md" />
+        <div className="flex flex-col justify-center">
           <p>current song</p>
           <p>Album</p>
         </div>
-      </div>
-      <div className="flex justify-center items-center flex-col">
-        <div className="space-x-4 flex">
-          <p>previous</p>
-          <p>pause</p>
-          <p>next</p>
+        <div className="flex items-center justify-center">
+          <CircleCheck className="fill-spotify stroke-black" />
         </div>
-        <Slider />
       </div>
-      <div>params</div>
+      <Player />
+      <div className="flex w-1/3 items-center justify-end">
+        <div className="flex w-1/2 justify-end space-x-1 pl-10 pr-2">
+          <Volume2 />
+          <Slider defaultValue={[0]} value={[0]} />
+        </div>
+      </div>
     </footer>
   )
 }
---
+++ src/components/ui/footer/player.tsx
@@ -0,0 +1,103 @@
+'use client'
+
+import {
+  CirclePause,
+  CirclePlay,
+  Repeat2,
+  Shuffle,
+  SkipBack,
+  SkipForward,
+} from 'lucide-react'
+import { useEffect, useRef, useState } from 'react'
+import { Slider } from '../slider'
+
+export default function Player() {
+  const [currentTime, setCurrentTime] = useState(0)
+  const [duration, setDuration] = useState(0)
+  const [audioUrl, setAudioUrl] = useState(
+    'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3',
+  )
+  const [isPlaying, setIsPlaying] = useState(false)
+  const audioRef = useRef<HTMLAudioElement | null>(null)
+  const [playerValue, setPlayerValue] = useState(0)
+
+  // useEffect(() => {
+  //   const fetchAudioUrl = async () => {
+  //     const response = await fetch('/api/song')
+  //     const data = await response.json()
+  //     setAudioUrl(data.url)
+  //   }
+  //   fetchAudioUrl()
+  // }, [])
+
+  useEffect(() => {
+    if (audioUrl) {
+      const audio = audioRef.current
+      if (audio) {
+        setDuration(audio.duration)
+      }
+    }
+  }, [])
+
+  const handleTimeUpdate = () => {
+    if (audioUrl) {
+      const audio = audioRef.current
+      if (audio) {
+        setCurrentTime(audio.currentTime)
+        setPlayerValue((audio.currentTime * 100) / audio.duration)
+      }
+    }
+  }
+
+  const playPause = () => {
+    const audio = audioRef.current
+    if (!audio) return
+    if (isPlaying) {
+      audio.pause()
+      // audio.currentTime = 0
+      // setCurrentTime(0)
+    } else {
+      audio.play()
+    }
+    setIsPlaying(!isPlaying)
+  }
+
+  const formatTime = (seconds: number) => {
+    const minutes = Math.floor(seconds / 60)
+    const secs = Math.floor(seconds % 60)
+    return `${minutes}:${secs < 10 ? '0' : ''}${secs}`
+  }
+
+  const handleValueChange = (value: number[]) => {
+    if (audioUrl) {
+      const audio = audioRef.current
+      if (audio) {
+        audio.currentTime = (value[0] * audio.duration) / 100
+        setPlayerValue(value[0])
+      }
+    }
+  }
+  return (
+    <section className="flex w-1/3 flex-col items-center justify-center 2xl:w-[800px]">
+      <div className="flex space-x-4">
+        <Shuffle />
+        <SkipBack />
+        <button onClick={playPause}>
+          {isPlaying ? <CirclePause /> : <CirclePlay />}
+        </button>
+        <SkipForward />
+        <Repeat2 />
+      </div>
+      <audio
+        ref={audioRef}
+        src={audioUrl}
+        onTimeUpdate={handleTimeUpdate}
+      ></audio>
+      <div className="flex w-full space-x-2">
+        <span>{formatTime(currentTime)}</span>
+        <Slider value={[playerValue]} defaultValue={[0]} onValueChange={handleValueChange} />
+        <span>{formatTime(duration)}</span>
+      </div>
+    </section>
+  )
+}
