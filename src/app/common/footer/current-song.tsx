2ebd0602a240d67211dcebc69821040b680731b0
leon3108 <maxime.noel@epitech.eu>
2024-05-28 22:14:55 +0200

    fix: display skeleton if url song not set

--- src/app/common/footer/current-song.tsx
+++ src/app/common/footer/current-song.tsx
@@ -4,6 +4,7 @@ import { CircleCheck } from 'lucide-react'
 import Image from 'next/image'
 import { useContext } from 'react'
 import { CurrentSongContext } from '../context/current-song'
+import SongSkeleton from './song-skeleton'
 
 export default function CurrentSong() {
   const currentSongContext = useContext(CurrentSongContext)
@@ -11,13 +12,17 @@ export default function CurrentSong() {
 
   return (
     <section className="flex w-1/3 space-x-2">
-      <Image
-        src={currentSongContext.currentSong.imageUrl}
-        alt="song's cover"
-        className="h-14 w-14 rounded-md"
-        width={56}
-        height={56}
-      />
+      {currentSongContext.currentSong.imageUrl == '' ? (
+        <SongSkeleton />
+      ) : (
+        <Image
+          src={currentSongContext.currentSong.imageUrl}
+          alt="song's cover"
+          className="h-14 w-14 rounded-md"
+          width={56}
+          height={56}
+        />
+      )}
       <div className="flex flex-col justify-center">
         <p>{currentSongContext.currentSong.songName}</p>
         <p>{currentSongContext.currentSong.artistName}</p>
