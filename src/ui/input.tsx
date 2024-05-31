eba2e53834980f40288e4d222d9ce4c575aaba5a
leon3108 <maxime.noel@epitech.eu>
2024-05-31 09:54:16 +0200

    improve input

--- src/ui/input.tsx
+++ src/ui/input.tsx
@@ -1,7 +1,5 @@
 import * as React from 'react'
 
-import { cn } from '@/lib/utils'
-
 export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {}
 
@@ -10,19 +8,19 @@ const Input = React.forwardRef<
   InputProps & { icon?: React.ReactNode }
 >(({ className, type, icon, ...props }, ref) => {
   return (
-    <div
-      className={cn(
-        'bg-black-500 flex h-12 w-full items-center rounded-3xl border border-input px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
-        className,
-      )}
-    >
-      {icon && <div className="mr-2">{icon}</div>}
+    <div className="relative flex h-12 w-96 items-center">
       <input
-        type={type}
-        ref={ref}
-        className="bg-black-500 placeholder:text-[#747475]"
+        type="text"
+        className="hover:bg-black-200 h-full w-full rounded-3xl border border-none bg-black-500 py-2 pl-14 pr-4 placeholder:text-[#747475] hover:border-[#414040] focus:outline-none focus:ring-2 focus:ring-white"
         {...props}
       />
+      <div className="pointer-events-none absolute left-0 flex items-center pl-3">
+        {icon && (
+          <div className="flex h-full items-center rounded-l-3xl  px-2 transition-colors duration-200 ease-in-out ">
+            {icon}
+          </div>
+        )}
+      </div>
     </div>
   )
 })
